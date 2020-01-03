e0b256e2ac77457e2f10e45230ee3e675cb50928
Andrew Engel <androidengel@gmail.com>
2020-01-03 14:58:59 -0600

    importing styled-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
+import styled from 'styled-components';
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Archive from './archive'
 import "./layout.css"
+
+const MainLayout = styled.main`
+  max-width: 90%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -28,22 +29,15 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-      <Archive />
+      <MainLayout>
+          <div>{children}</div>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        <Archive />
+      </MainLayout>
     </>
   )
 }
